77dd98f30dcd493cd924725a14d3a2eaa15a4793
goro9 <sokenbiccha@icloud.com>
2020-03-22 12:10:52 +0900

    done but exist any type

---
+++ src/components/List.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+let style = {
+  maxWidth: '700px',
+};
+
+let btn = {
+  cursor: 'pointer'
+};
+
+interface ListPropsInterface {
+  todos: any[];
+  handleRemove: any;
+}
+
+const List = (props: ListPropsInterface) => (
+  <ul className="siimple-list">
+    {props.todos.map((todo, i) => {
+      return <li key={i} className="siimple-list-item siimple--bg-white" style={style}>{todo.title} <span className="siimple-tag siimple-tag--error siimple-hover" style={btn} onClick={() => props.handleRemove(i)}>Delete</span></li>
+    })};
+  </ul>
+);
+
+export default List;
